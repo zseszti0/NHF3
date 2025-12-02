93a9dde02858e0e996e032166fc1c05860156211
zseszti0 <zseszti42.3@gmail.com>
2025-12-02 02:27:16 +0100

    all done except: reaction animation (dummy version)

--- src/main/java/graphics/MainMenuScene.java
+++ src/main/java/graphics/MainMenuScene.java
@@ -7,6 +7,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.geometry.Pos;
 
@@ -24,6 +25,12 @@ public class MainMenuScene extends BaseScene{
 
     @Override
     protected StackPane createContainer() {
+        //mainFont
+        Font mainFont = Font.loadFont(
+                getClass().getResource("/fonts/main.ttf").toExternalForm(),
+                32
+        );
+
         //root container of all objects
         StackPane root = new StackPane();
 
@@ -35,19 +42,19 @@ public class MainMenuScene extends BaseScene{
 
         //3 menu buttons
         VBox buttonCont = new VBox();
-        buttonCont.setPrefHeight(400);
+        buttonCont.setPrefHeight(700);
         buttonCont.setPrefWidth(200);
 
         //init the buttons
-        Button startButton = new Button("Mix!");
+        Button startButton = MixScene.setupControlButtonGraphics(mainFont, "Mix!",154,95,"/assets/ui/controlButtonHover/buttonBase_00033.png");
         startButton.setOnAction(e -> SceneManager.loadScene("mix")); //temporary action
 
-        Button recipesButton = new Button("Recipes");
+        Button recipesButton = MixScene.setupControlButtonGraphics(mainFont, "Recipes",154,95,"/assets/ui/controlButtonHover/buttonBase_00033.png");
         recipesButton.setOnAction(e -> {
             RecipesPane recipesPane = new RecipesPane(root, isPaneOpen -> {});
         });
 
-        Button exitButton = new Button("Exit");
+        Button exitButton = MixScene.setupControlButtonGraphics(mainFont, "Exit",154,95,"/assets/ui/controlButtonHover/buttonBase_00033.png");
         exitButton.setOnAction(e -> System.exit(0));
 
         buttonCont.getChildren().addAll(startButton, recipesButton, exitButton);
@@ -61,7 +68,7 @@ public class MainMenuScene extends BaseScene{
         buttonContPane.setPrefSize(BaseScene.WIDTH, BaseScene.HEIGHT);
 
         buttonContPane.getChildren().add(buttonCont);
-        buttonCont.setLayoutX(20);
+        buttonCont.setLayoutX(150);
         buttonCont.layoutYProperty().bind(
                 buttonContPane.heightProperty()
                         .subtract(buttonCont.heightProperty())
